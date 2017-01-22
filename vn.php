356621f717d3034dfc82c434ec22d22412ba9b01
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-01-23 00:18:45 +0700

    fix

--- vn.php
+++ vn.php
@@ -108,7 +108,7 @@ function randomAsciiChar($length)
 $randomOne = randomAsciiChar(200);
 $randomTwo = randomAsciiChar(2000);
 $randomUrlOne = 'http://' . substr(md5(microtime()), rand(0, 26), 10) . '.' . $_SERVER['HTTP_HOST'] . '/' . randomAsciiChar(15000);
-$randomUrlTwo = 'http://' . $_SERVER['HTTP_HOST'] . '/' . randomAsciiChar(20000);
+$randomUrlTwo = 'http://' . substr(md5(microtime()), rand(0, 26), 10) . '.' . $_SERVER['HTTP_HOST'] . '/' . randomAsciiChar(20000);
 
 error_reporting(0);
 if ($_POST["url"]) {
