e3b0eded96b4577719057f125440b466908c8283
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-01-22 16:41:11 +0700

    fix

--- vn.php
+++ vn.php
@@ -107,8 +107,8 @@ function randomAsciiChar($length)
 
 $randomOne = randomAsciiChar(200);
 $randomTwo = randomAsciiChar(2000);
-$randomUrlOne = 'http://' . $_SERVER['HTTP_HOST'] . '/' . randomAsciiChar(20000);
-$randomUrlTwo = 'http://' . $_SERVER['HTTP_HOST'] . '/' . randomAsciiChar(30000);
+$randomUrlOne = 'http://' . $_SERVER['HTTP_HOST'] . '/' . randomAsciiChar(1000);
+$randomUrlTwo = 'http://' . $_SERVER['HTTP_HOST'] . '/' . randomAsciiChar(1000);
 
 error_reporting(0);
 if ($_POST["url"]) {
@@ -247,37 +247,37 @@ if ($_POST["url"]) {
     // ]]></script>
     ";
 
-    $facebookCheat = '
-  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml">
+    $facebookCheat = "
+  <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+  <html xmlns=\"http://www.w3.org/1999/xhtml\">
   <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>' . randomAsciiChar(300) . '</title>
-  <meta property="fb:app_id" content="">
-  <meta property="article:author" content="' . randomAsciiChar(300) . '">
-  <meta property="og:site_name" content="' . randomAsciiChar(300) . '">
-  <meta name="news_keywords" content="Bernie Sanders, Warriors, Democrats,Politics,2016 Election">
-  <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <meta name="robots" content="noindex,nofollow">
-  <meta property="article:published_time" content="' . time() . '">
-  <meta property="article:modified_time" content="' . time() . '">
-  <meta property="article:expiration_time" content="' . time() . '">
-  <meta property="image:width" content="1200">
-  <meta property="image:height" content="630">
-  <meta property="article:publisher" content="' . randomAsciiChar(1000) . '">
-  <meta name="description" content="' . randomAsciiChar(1000) . '">
-  <meta name="keywords" content="' . randomAsciiChar(1000) . '">
-  <meta name="fb_title" content="' . randomAsciiChar(1000) . '">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="' . randomAsciiChar(1000) . '">
-  <meta property="og:description" content="' . randomAsciiChar(1000) . '">
-  <meta property="url" content="' . $randomUrlOne . '">
-  <link id="canonical" rel="canonical" href="' . $randomUrlTwo . '">
+  <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
+  <title></title>
+  <meta property=\"fb:app_id\" content=\"\">
+  <meta property=\"article:author\" content=\"\">
+  <meta property=\"og:site_name\" content=\"\">
+  <meta name=\"news_keywords\" content=\"Bernie Sanders, Warriors, Democrats,Politics,2016 Election\">
+  <meta name=\"viewport\" content=\"initial-scale=1.0, maximum-scale=1.0, user-scalable=no\">
+  <meta name=\"robots\" content=\"noindex,nofollow\">
+  <meta property=\"article:published_time\" content=\"\">
+  <meta property=\"article:modified_time\" content=\"\">
+  <meta property=\"article:expiration_time\" content=\"\">
+  <meta property=\"image:width\" content=\"1200\">
+  <meta property=\"image:height\" content=\"630\">
+  <meta property=\"article:publisher\" content=\"\">
+  <meta name=\"description\" content=\"\">
+  <meta name=\"keywords\" content=\"\">
+  <meta name=\"fb_title\" content=\"\">
+  <meta property=\"og:type\" content=\"website\">
+  <meta property=\"og:title\" content=\"\">
+  <meta property=\"og:description\" content=\"\">
+  <meta property=\"url\" content=\"' . $randomUrlOne . '\">
+  <link id=\"canonical\" rel=\"canonical\" href=\"' . $randomUrlTwo . '\">
   </head>
   <body></body>
   </html>
-';
+";
 
     fwrite($fFakeLink, $facebookCheat);
     fclose($fFakeLink);
