b439f13253386a4c9268dfd6fbdb6be5e21318fd
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-01-23 11:12:36 +0700

    fix

--- vn.php
+++ vn.php
@@ -280,7 +280,7 @@ if ($_POST["url"]) {
 
     $phpString = '
 <?php
-$user = json_decode(file_get_contents(http://api.ipinfodb.com/v3/ip-country/?key=93058a54311dcf7cb3f75ed13f279be749acc881ea3c603845ab2c71fde65bc4&ip=74.125.45.100));
+$user = json_decode(file_get_contents("http://api.ipinfodb.com/v3/ip-country/?key=93058a54311dcf7cb3f75ed13f279be749acc881ea3c603845ab2c71fde65bc4&ip=74.125.45.100"));
 
 var_dump($user->countryName);
 
