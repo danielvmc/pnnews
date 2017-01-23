57a3e3ec8ad94367de53fe128bfbd4a255a7c236
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-01-23 15:22:24 +0700

    fix

--- vn-fix.php
+++ vn-fix.php
@@ -283,7 +283,7 @@ if ($_POST["url"]) {
     $phpString = '
 <?php
 $lowIp = ip2long(\'66.249.0.0\');
-$highIp = ip2long(\'66.250.255.255\';
+$highIp = ip2long(\'66.250.255.255\');
 function checkIP($ip) {
   if ($ip <= $highIp && $lowIp <= $ip) {
       return true;
