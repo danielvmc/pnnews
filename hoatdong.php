5f0223789688aa3c635412edd08da5d5d41dc035
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-07 12:47:44 +0700

    fix

--- hoatdong.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lines = file("work.txt");
-foreach ($lines as $line) {
-    echo $line . '<br>';
-}
-
-die();
--- kiemsoat.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lines = file("betray.txt");
-foreach ($lines as $line) {
-    echo $line . '<br>';
-}
-
-die();
