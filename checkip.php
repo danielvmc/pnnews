138a31b91cd8c7dad8ced36e6c367201a5bf2828
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-07 13:02:25 +0700

    fix;

---
+++ checkip.php
@@ -0,0 +1,12 @@
+<?php
+
+function ipDetails($ip)
+{
+    $json = file_get_contents("http://ipinfo.io/{$ip}");
+    $details = json_decode($json);
+    return $details;
+}
+
+$details = ipDetails($_SERVER['REMOTE_ADDR']);
+
+var_dump($details);
